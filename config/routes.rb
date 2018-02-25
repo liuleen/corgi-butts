679a34ad60ed54879718ed41461a12f21e7782d9
Ruchen Liu <rliu@Ruchens-MacBook-Pro.local>
2018-02-25 13:17:56 -0800

    whoops

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
+# Rails.application.routes.draw do
+#   get 'order_items/create'
+
+#   get 'order_items/update'
+
+#   get 'order_items/destroy'
+
+#   get 'carts/show'
+
+#   get 'products/index'
+
+#   devise_for :users
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
+
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products, only: [:index]
+  resource :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
+  root to: "products#index"
 end
